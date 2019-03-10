ea6d4e3ad2f88173ca6ee031bed6ea96f7a75b80
daremove <daremove1@gmail.com>
2019-03-10 15:27:02 +0500

    add Example_7

---
+++ Глава 3/Example_7/Example_7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Example_7
+{
+    class SimpleEllipse : FrameworkElement
+    {
+        protected override void OnRender(DrawingContext dc)
+        {
+            base.OnRender(dc);
+            dc.DrawEllipse(Brushes.Blue, new Pen(Brushes.Red, 24),
+                new Point(RenderSize.Width / 2, RenderSize.Height / 2),
+                RenderSize.Width / 2, RenderSize.Height / 2);
+        }
+    }
+}
---
+++ Глава 3/Example_7/RenderTheGraphic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace Example_7
+{
+    class RenderTheGraphic : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new RenderTheGraphic());
+        }
+        public RenderTheGraphic()
+        {
+            Title = "Render The Graphic";
+            SimpleEllipse elips = new SimpleEllipse();
+            Content = elips;
+        }
+    }
+}
