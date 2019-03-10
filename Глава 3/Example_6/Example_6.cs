7c0af7647af699ed14d3c02f1bc6955c1db67bae
daremove <daremove1@gmail.com>
2019-03-10 15:01:16 +0500

    add Example_6

---
+++ Глава 3/Example_6/Example_6.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Documents;
+
+namespace Example_6
+{
+    public class ToggleBoldAndItalic : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new ToggleBoldAndItalic());
+        }
+        public ToggleBoldAndItalic()
+        {
+            Title = "Toggle Bold & Italic";
+            TextBlock text = new TextBlock();
+            text.FontSize = 32;
+            text.HorizontalAlignment = HorizontalAlignment.Center;
+            text.VerticalAlignment = VerticalAlignment.Center;
+            Content = text;
+
+            string strQuote = "To be, or not to be, that is the question";
+            string[] strWords = strQuote.Split();
+
+            foreach (string str in strWords)
+            {
+                Run run = new Run(str);
+                run.MouseDown += RunOnMouseDown;
+                text.Inlines.Add(run);
+                text.Inlines.Add(" ");
+            }
+        }
+        void RunOnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Run run = sender as Run;
+
+            if (e.ChangedButton == MouseButton.Left)
+                run.FontStyle = run.FontStyle == FontStyles.Italic ?
+                    FontStyles.Normal : FontStyles.Italic;
+
+            if (e.ChangedButton == MouseButton.Right)
+                run.FontWeight = run.FontWeight == FontWeights.Bold ?
+                    FontWeights.Normal : FontWeights.Bold;
+        }
+    }
+}
