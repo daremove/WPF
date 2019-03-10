e32742d6aeeabdf7d9903b3c279d34440ff0ee30
daremove <daremove1@gmail.com>
2019-03-10 13:40:20 +0500

    init

---
+++ Глава 3/Example_1/Example_1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Example_1
+{
+    public class DisplaySomeText : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new DisplaySomeText());
+        }
+        public DisplaySomeText()
+        {
+            //Задаем шрифт
+            FontFamily = new FontFamily("Times New Roman");
+            //Задаем размер шрифта, начертание и толщину
+            FontSize = 32;
+            FontStyle = FontStyles.Italic;
+            FontWeight = FontWeights.Bold;
+
+            Title = "Display Some Text";
+            //Пример того, как свойству Content можно задать текстовую строку
+            Content = "Content can be simple text!";
+        }
+    }
+}
---
+++ Глава 3/Example_2/Example_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Example_1
+{
+    public class DisplaySomeText : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new DisplaySomeText());
+        }
+        public DisplaySomeText()
+        {
+            Title = "Display Some Text";
+            Content = "Content can be simple text!";
+        }
+    }
+}
