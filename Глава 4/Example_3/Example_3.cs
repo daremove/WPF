3d3a489778ffc1dd944e178b67acdb2a6ed31187
daremove <daremove1@gmail.com>
2019-03-10 20:33:56 +0500

    add Example_3

---
+++ Глава 4/Example_3/Example_3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Example_3
+{
+    public class ImageTheButton : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new ImageTheButton());
+        }
+        public ImageTheButton()
+        {
+            Title = "Image the Button";
+            Uri uri = new Uri("pack://application:,,,/catbread.jpg");
+            BitmapImage bitmap = new BitmapImage(uri);
+            Image img = new Image();
+            img.Source = bitmap;
+            img.Stretch = Stretch.None;
+
+            Button btn = new Button();
+            btn.Content = img;
+            btn.HorizontalAlignment = HorizontalAlignment.Center;
+            btn.VerticalAlignment = VerticalAlignment.Center;
+
+            Content = btn;
+        }
+    }
+    
+}
