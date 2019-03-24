3cfa842a64968ba379b95558fe78a6f3b6a60c51
daremove <daremove1@gmail.com>
2019-03-24 11:30:12 +0500

    add Example_6

---
+++ Глава 5/Example_6/Example_6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Example_6
+{
+    public class ExploreDirectories : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new ExploreDirectories());
+        }
+        public ExploreDirectories()
+        {
+            Title = "Explore Directories";
+
+            ScrollViewer scroll = new ScrollViewer();
+            Content = scroll;
+
+            WrapPanel wrap = new WrapPanel();
+            scroll.Content = wrap;
+
+            wrap.Children.Add(new FileSystemInfoButton());
+        }
+    }
+}
---
+++ Глава 5/Example_6/FileSystemInfoButton.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Example_6
+{
+    public class FileSystemInfoButton : Button
+    {
+        FileSystemInfo info;
+
+        public FileSystemInfoButton() : this(new DirectoryInfo(
+            Environment.GetFolderPath(
+                Environment.SpecialFolder.MyDocuments)))
+        {}
+        public FileSystemInfoButton(FileSystemInfo info)
+        {
+            this.info = info;
+            Content = info.Name;
+
+            if (info is DirectoryInfo)
+                FontWeight = FontWeights.Bold;
+            Margin = new Thickness(10);
+        }
+        public FileSystemInfoButton(FileSystemInfo info, string str) : this(info) {
+            Content = str;
+        }
+        protected override void OnClick()
+        {
+            if (info is FileInfo)
+            {
+                Process.Start(info.FullName);
+            }
+            else if (info is DirectoryInfo) {
+                DirectoryInfo dir = info as DirectoryInfo;
+                Application.Current.MainWindow.Title = dir.FullName;
+
+                Panel pnl = Parent as Panel;
+                pnl.Children.Clear();
+
+                if (dir.Parent != null)
+                    pnl.Children.Add(new FileSystemInfoButton(dir.Parent, ".."));
+                foreach (FileSystemInfo inf in dir.GetFileSystemInfos())
+                    pnl.Children.Add(new FileSystemInfoButton(inf));
+            }
+
+            base.OnClick();
+        }
+    }
+}
