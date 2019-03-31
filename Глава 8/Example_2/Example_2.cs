b3b8d85374098f31f88e254b314856e9009ec119
daremove <daremove1@gmail.com>
2019-03-31 13:23:54 +0500

    add Example_2

---
+++ Глава 8/Example_2/Example_2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Example_2
+{
+    public class SpaceButton : Button
+    {
+        string txt;
+
+        public string Text
+        {
+            set
+            {
+                txt = value;
+                Content = SpaceOutText(txt);
+            }
+            get
+            {
+                return txt;
+            }
+        }
+
+        public static readonly DependencyProperty SpaceProperty;
+
+        public int Space
+        {
+            set
+            {
+                SetValue(SpaceProperty, value);
+            }
+            get
+            {
+                return (int)GetValue(SpaceProperty);
+            }
+        }
+
+        static SpaceButton()
+        {
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata();
+            metadata.DefaultValue = 1;
+            metadata.AffectsMeasure = true;
+            metadata.Inherits = true;
+            metadata.PropertyChangedCallback += OnSpacePropertyChanged;
+
+            SpaceProperty = DependencyProperty.Register("Space", typeof(int), typeof(SpaceButton), 
+                metadata, ValidateSpaceValue);
+        }
+
+        static bool ValidateSpaceValue(object obj)
+        {
+            int i = (int)obj;
+            return i >= 0;
+        }
+
+        static void OnSpacePropertyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            SpaceButton btn = obj as SpaceButton;
+            btn.Content = btn.SpaceOutText(btn.Text);
+        }
+
+        string SpaceOutText(string str)
+        {
+            if (str == null)
+                return null;
+
+            StringBuilder build = new StringBuilder();
+            foreach (char ch in str)
+                build.Append(ch + new string(' ', Space));
+
+            return build.ToString();
+        }
+    }
+}
