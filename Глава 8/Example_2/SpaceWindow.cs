e0fc6ef2f5b24a1a244a38ca73a965fe4beebd74
daremove <daremove1@gmail.com>
2019-03-31 13:45:00 +0500

    update Example_2

---
+++ Глава 8/Example_2/SpaceWindow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Example_2
+{
+    public class SpaceWindow : Window
+    {
+        public static readonly DependencyProperty SpaceProperty;
+        public int Space
+        {
+            set
+            {
+                SetValue(SpaceProperty, value);
+            }
+            get
+            {
+                return (int)GetValue(SpaceProperty);
+            }
+        }
+
+        static SpaceWindow()
+        {
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata();
+            metadata.Inherits = true;
+
+            SpaceProperty = SpaceButton.SpaceProperty.AddOwner(typeof(SpaceWindow));
+            SpaceProperty.OverrideMetadata(typeof(SpaceWindow), metadata);
+        }
+    }
+}
